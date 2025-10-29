3c430c9282748e21c561d055c95b3a13e4a9abb9
XiaopengJi-USQ <97103045+XiaopengJi-USQ@users.noreply.github.com>
2025-10-29 23:52:04 +1000

    Update DG.py

--- model/DG.py
+++ model/DG.py
@@ -21,16 +21,21 @@ class kFoldGenerator():
     # Get i-th fold
     def getFold(self, i):
         isFirst = True
+        val_id_list = [2,5,0,1,7,1,7,3,4,8]
         for p in range(self.k):
             if p != i:
-                if isFirst:
-                    train_data = self.x_list[p]
-                    train_targets = self.y_list[p]
-                    isFirst = False
+                if p == val_id_list[i]:
+                    val_data = self.x_list[p]
+                    val_targets = self.y_list[p]
                 else:
-                    train_data = np.concatenate((train_data, self.x_list[p]))
-                    train_targets = np.concatenate((train_targets, self.y_list[p]))
+                    if isFirst:
+                        train_data = self.x_list[p]
+                        train_targets = self.y_list[p]
+                        isFirst = False
+                    else:
+                        train_data = np.concatenate((train_data, self.x_list[p]))
+                        train_targets = np.concatenate((train_targets, self.y_list[p]))
             else:
-                val_data = self.x_list[p]
-                val_targets = self.y_list[p]
-        return train_data, train_targets, val_data, val_targets
+                test_data = self.x_list[p]
+                test_targets = self.y_list[p]
+        return train_data, train_targets, val_data, val_targets,test_data,test_targets
